98a30631f3645e1b5694c879ae941abcb1a7e71d
Patbateman09 <135843247+Patbateman09@users.noreply.github.com>
2023-09-18 23:31:20 +0530

    Add files via upload

---
+++ news.js
@@ -0,0 +1,18 @@
+function whcolor(){
+    document.bgColor="white";
+}
+function redcolor(){
+    document.bgColor="red";
+}
+function grncolor(){
+    document.bgColor="green";
+}
+function cyancolor(){
+    document.bgColor="cyan";
+}
+function blackcolor(){
+    document.bgColor="black";
+}
+function yllwcolor(){
+    document.bgColor="yellow";
+}
